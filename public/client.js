2366b6d12b444f135bbcbabd37309f07ad75209e
System <none>
2017-09-26 12:39:05 +0000

    🏋🍩 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -23,4 +23,45 @@ $(function() {
     });
   });
 
+
 });
+
+
+  window.onload = function() {
+		// Get a reference to the canvas object
+		var canvas = document.getElementById('venny');
+		// Create an empty project and a view for the canvas:
+		paper.setup(canvas);
+
+    var opacity = 0.6;
+    var fontSize = 18;
+    var textcolor = 'white';
+    
+    var myCircle = new paper.Path.Circle(new paper.Point(200, 200), 180);
+    myCircle.strokeColor = 'blue';
+    myCircle.fillColor = 'blue';
+    myCircle.opacity = opacity;
+    
+    var otherCircle = new paper.Path.Circle(new paper.Point(400, 200), 180);
+    otherCircle.strokeColor = 'red';
+    otherCircle.fillColor = 'red';
+    otherCircle.opacity = opacity;
+    
+    var text = new paper.PointText(new paper.Point(150, 200));
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.content = 'Gases';
+    text.fontSize = 18;
+    
+    var text = new paper.PointText(new paper.Point(300, 200));
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 18;
+    text.content = 'Farts';
+    
+    var text = new paper.PointText(new paper.Point(450, 200));
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 18;
+    text.content = 'Poops';
+	}
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // server.js
 // where your node app starts
 
+// WHY DID I MAKE THIS A NODE APP? SHUT UP, NERDS.
+
 // init project
 var express = require('express');
 var app = express();
@@ -15,24 +17,6 @@ app.use(express.static('public'));
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes",
-  "Do a fart."
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
