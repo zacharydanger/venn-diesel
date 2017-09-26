272fe17167739290e4a34a9a0981b2ab438bba57
System <none>
2017-09-26 13:05:03 +0000

    🐠💕 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -4,64 +4,62 @@
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
 
+var vennIt = function(canvas, a, intersection, b) {
+  paper.setup(canvas);
+  
+  var opacity = 0.6;
+  var fontSize = 24;
+  var textcolor = 'white';
+    
+  var myCircle = new paper.Path.Circle(new paper.Point(200, 200), 180);
+  myCircle.strokeColor = 'blue';
+  myCircle.fillColor = 'blue';
+  myCircle.opacity = opacity;
+    
+  var otherCircle = new paper.Path.Circle(new paper.Point(400, 200), 180);
+  otherCircle.strokeColor = 'red';
+  otherCircle.fillColor = 'red';
+  otherCircle.opacity = opacity;
+    
+  var text = new paper.PointText(new paper.Point(150, 200));
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.content = a;
+  text.fontSize = fontSize;
+    
+  var text = new paper.PointText(new paper.Point(300, 200));
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = fontSize;
+  text.content = intersection;
+    
+  var text = new paper.PointText(new paper.Point(450, 200));
+  text.justification = 'center';
+  text.fillColor = 'white';
+  text.fontSize = fontSize;
+  text.content = b;
+}
+
 $(function() {
   console.log('hello world :o');
   
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
-  $('form').submit(function(event) {
+  // Get a reference to the canvas object
+	var canvas = document.getElementById('venny');
+    
+	// Create an empty project and a view for the canvas:
+	paper.setup(canvas);
+    
+  //vennIt(canvas, "Gases", "Farts", "Butts");
+  
+  $('#and-the-jets').submit(function(event) {
     event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
+    
+    var A = $('input[name="A"]').val();
+    var B = $('input[name="B"]').val();
+    var C = $('input[name="intersection"]').val();
+    
+    vennIt(canvas, A, C, B);
   });
-
-
+  
+  $("#and-the-jets").submit();
 });
-
-
-  window.onload = function() {
-		// Get a reference to the canvas object
-		var canvas = document.getElementById('venny');
-		// Create an empty project and a view for the canvas:
-		paper.setup(canvas);
-
-    var opacity = 0.6;
-    var fontSize = 18;
-    var textcolor = 'white';
-    
-    var myCircle = new paper.Path.Circle(new paper.Point(200, 200), 180);
-    myCircle.strokeColor = 'blue';
-    myCircle.fillColor = 'blue';
-    myCircle.opacity = opacity;
-    
-    var otherCircle = new paper.Path.Circle(new paper.Point(400, 200), 180);
-    otherCircle.strokeColor = 'red';
-    otherCircle.fillColor = 'red';
-    otherCircle.opacity = opacity;
-    
-    var text = new paper.PointText(new paper.Point(150, 200));
-    text.justification = 'center';
-    text.fillColor = 'white';
-    text.content = 'Gases';
-    text.fontSize = 18;
-    
-    var text = new paper.PointText(new paper.Point(300, 200));
-    text.justification = 'center';
-    text.fillColor = 'white';
-    text.fontSize = 18;
-    text.content = 'Farts';
-    
-    var text = new paper.PointText(new paper.Point(450, 200));
-    text.justification = 'center';
-    text.fillColor = 'white';
-    text.fontSize = 18;
-    text.content = 'Poops';
-	}
